e48f88d9a0d305c8db63f883a7ee6e57506aeac8
Lincan Li <email@lincan.li>
2022-04-09 15:42:50 +0800

    add debounce sequence

---
+++ async_debounce_sequence.go
@@ -0,0 +1,109 @@
+package co
+
+import (
+	"sync"
+	"time"
+
+	"github.com/tempura-shrimp/co/ds/queue"
+	co_sync "github.com/tempura-shrimp/co/sync"
+)
+
+type AsyncDebounceSequence[R any] struct {
+	*asyncSequence[R]
+
+	previousIterator Iterator[R]
+	interval         time.Duration
+	tolerance        time.Duration
+}
+
+func NewAsyncDebounceSequence[R any](it AsyncSequenceable[R], interval time.Duration) *AsyncDebounceSequence[R] {
+	a := &AsyncDebounceSequence[R]{
+		previousIterator: it.iterator(),
+		interval:         interval,
+	}
+	a.asyncSequence = NewAsyncSequence[R](a)
+	return a
+}
+
+func (a *AsyncDebounceSequence[R]) SetInterval(interval time.Duration) *AsyncDebounceSequence[R] {
+	a.interval = interval
+	return a
+}
+
+func (a *AsyncDebounceSequence[R]) SetTolerance(tolerance time.Duration) *AsyncDebounceSequence[R] {
+	a.tolerance = a.interval + tolerance
+	return a
+}
+
+func (c *AsyncDebounceSequence[R]) iterator() Iterator[R] {
+	it := &asyncDebounceSequenceIterator[R]{
+		AsyncDebounceSequence: c,
+		bufferedData:          queue.NewQueue[R](),
+		bufferWait:            sync.NewCond(&sync.Mutex{}),
+	}
+	it.asyncSequenceIterator = NewAsyncSequenceIterator[R](it)
+	return it
+}
+
+type asyncDebounceSequenceIterator[R any] struct {
+	*asyncSequenceIterator[R]
+
+	*AsyncDebounceSequence[R]
+
+	previousTime time.Time
+	bufferedData *queue.Queue[R]
+
+	runOnce     sync.Once
+	sourceEnded bool
+	bufferWait  *sync.Cond
+}
+
+func (it *asyncDebounceSequenceIterator[R]) intervalPassed() bool {
+	return time.Since(it.previousTime) > it.interval
+}
+
+func (it *asyncDebounceSequenceIterator[R]) tolerancePassed() bool {
+	return time.Since(it.previousTime) > it.tolerance
+}
+
+func (it *asyncDebounceSequenceIterator[R]) startBuffer() {
+	it.runOnce.Do(func() {
+		it.previousTime = time.Now()
+
+		co_sync.SafeGo(func() {
+			for op, err := it.previousIterator.next(); op.valid; op, err = it.previousIterator.next() {
+				if err != nil {
+					continue
+				}
+				reachedInterval := it.intervalPassed()
+				if !reachedInterval {
+					continue
+				}
+
+				it.bufferedData.Enqueue(op.data)
+				if it.tolerance == it.interval || it.tolerancePassed() {
+					co_sync.CondBoardcast(it.bufferWait, func() { it.previousTime = time.Now() })
+				}
+			}
+			co_sync.CondBoardcast(it.bufferWait, func() { it.sourceEnded = true })
+		})
+	})
+}
+
+func (it *asyncDebounceSequenceIterator[R]) next() (*Optional[R], error) {
+	it.startBuffer()
+
+	if it.sourceEnded && it.bufferedData.Len() == 0 {
+		return NewOptionalEmpty[R](), nil
+	}
+
+	co_sync.CondWait(it.bufferWait, func() bool {
+		return !it.sourceEnded && it.bufferedData.Len() == 0
+	})
+
+	if it.sourceEnded && it.bufferedData.Len() == 0 {
+		return NewOptionalEmpty[R](), nil
+	}
+
+	return OptionalOf(it.bufferedData.Dequeue()), nil
+}
---
+++ async_debounce_sequence_test.go
@@ -0,0 +1,65 @@
+package co_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/tempura-shrimp/co"
+)
+
+func TestAsyncDebounceSequence(t *testing.T) {
+	convey.Convey("given a sequential int", t, func() {
+		queued := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
+		sourceCh := make(chan int)
+
+		oChannel := co.FromChan(sourceCh)
+		dList := co.NewAsyncDebounceSequence[int](oChannel, time.Second)
+
+		go func() {
+			time.Sleep(time.Second)
+			for _, val := range queued {
+				sourceCh <- val
+				time.Sleep(time.Millisecond * 100)
+			}
+			oChannel.Done()
+		}()
+
+		convey.Convey("expect resolved list to be identical with given values", func() {
+			expected := []int{1, 11, 21}
+			actual := []int{}
+			for data := range dList.Emit() {
+				actual = append(actual, data.GetValue())
+			}
+			convey.So(actual, convey.ShouldResemble, expected)
+		})
+	})
+}
+
+func TestAsyncDebounceSequenceTolerance(t *testing.T) {
+	convey.Convey("given a sequential int", t, func() {
+		queued := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
+		sourceCh := make(chan int)
+
+		oChannel := co.FromChan(sourceCh)
+		dList := co.NewAsyncDebounceSequence[int](oChannel, time.Second).SetTolerance(time.Millisecond * 200)
+
+		go func() {
+			time.Sleep(time.Second)
+			for _, val := range queued {
+				sourceCh <- val
+				time.Sleep(time.Millisecond * 100)
+			}
+			oChannel.Done()
+		}()
+
+		convey.Convey("expect resolved list to be identical with given values", func() {
+			expected := []int{1, 2, 3, 13, 14, 15, 25}
+			actual := []int{}
+			for data := range dList.Emit() {
+				actual = append(actual, data.GetValue())
+			}
+			convey.So(actual, convey.ShouldResemble, expected)
+		})
+	})
+}
