61e72b42a0603010d84a8d52974595115958a505
Lincan Li <email@lincan.li>
2022-04-19 02:04:50 +0800

    implemented any sequence

---
+++ async_any_sequence.go
@@ -0,0 +1,77 @@
+package co
+
+import (
+	"sync"
+
+	"tempura.ink/co/ds/queue"
+	co_sync "tempura.ink/co/internal/sync"
+)
+
+type AsyncAnySequence[R any] struct {
+	*asyncSequence[R]
+
+	its []Iterator[R]
+}
+
+func NewAsyncAnySequence[R any](its ...AsyncSequenceable[R]) *AsyncAnySequence[R] {
+	a := &AsyncAnySequence[R]{
+		its: toAsyncIterators(its...),
+	}
+	a.asyncSequence = NewAsyncSequence[R](a)
+	return a
+}
+
+func (c *AsyncAnySequence[R]) iterator() Iterator[R] {
+	it := &asyncAnySequenceIterator[R]{
+		AsyncAnySequence: c,
+		dataQueue:        queue.NewQueue[R](),
+	}
+	it.asyncSequenceIterator = NewAsyncSequenceIterator[R](it)
+	return it
+}
+
+type asyncAnySequenceIterator[R any] struct {
+	*asyncSequenceIterator[R]
+
+	*AsyncAnySequence[R]
+	dataQueue   *queue.Queue[R]
+	sourceEnded bool
+}
+
+func (it *asyncAnySequenceIterator[R]) next() *Optional[R] {
+	if it.sourceEnded {
+		return NewOptionalEmpty[R]()
+	}
+	if it.dataQueue.Len() != 0 {
+		return OptionalOf(it.dataQueue.Dequeue())
+	}
+
+	completedCh := make(chan bool)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(it.its))
+
+	for i, pIt := range it.its {
+		go func(idx int, pIt Iterator[R]) {
+			defer wg.Done()
+			for op := pIt.next(); op.valid; op = pIt.next() {
+				it.dataQueue.Enqueue(op.data)
+				co_sync.SafeSend(completedCh, true)
+				return
+			}
+			co_sync.SafeSend(completedCh, true)
+		}(i, pIt)
+	}
+
+	<-completedCh
+	close(completedCh)
+
+	if it.dataQueue.Len() == 0 {
+		if wg.Wait(); it.dataQueue.Len() == 0 {
+			it.sourceEnded = true
+			return NewOptionalEmpty[R]()
+		}
+	}
+
+	return OptionalOf(it.dataQueue.Dequeue())
+}
---
+++ async_any_sequence_test.go
@@ -0,0 +1,45 @@
+package co_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/smartystreets/goconvey/convey"
+	"golang.org/x/exp/slices"
+	"tempura.ink/co"
+)
+
+func TestAsyncAnySequence(t *testing.T) {
+	convey.Convey("given a sequential int", t, func() {
+		queued1 := []int{1, 3, 5, 7, 9}
+		sourceCh1 := make(chan int)
+		oChannel1 := co.FromChan(sourceCh1)
+
+		queued2 := []int{2, 4, 6, 8, 10}
+		sourceCh2 := make(chan int)
+		oChanne2 := co.FromChan(sourceCh2)
+
+		bList := co.NewAsyncAnySequence[int](oChannel1, oChanne2)
+
+		go func() {
+			time.Sleep(time.Second)
+			for i := range queued1 {
+				sourceCh1 <- queued1[i]
+				time.Sleep(time.Millisecond * 100)
+				sourceCh2 <- queued2[i]
+			}
+			oChannel1.Complete()
+			oChanne2.Complete()
+		}()
+
+		convey.Convey("expect resolved list to be identical with given values", func() {
+			expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+			actual := []int{}
+			for data := range bList.Iter() {
+				actual = append(actual, data)
+			}
+			slices.Sort(actual)
+			convey.So(actual, convey.ShouldResemble, expected)
+		})
+	})
+}
